087dccacb1ba23079d1e98bf4cabbf4080de95b5
ikReza <ireza.kaiser00@gmail.com>
2020-07-18 22:14:38 +0600

    breadcrumb color change

--- portfolio-1/src/components/ProjectNavigation.jsx
+++ portfolio-1/src/components/ProjectNavigation.jsx
@@ -7,7 +7,7 @@ const useStyles = makeStyles(() => ({
     display: "flex",
     alignItems: "center",
     justifyConte: "center",
-    backgroundColor: "gray",
+    backgroundColor: "#272727",
   },
 }));
 
@@ -16,23 +16,25 @@ const ProjectNavigation = () => {
   return (
     <AppBar position="sticky" className={classes.stickyApp}>
       <Toolbar variant="dense">
-        <Breadcrumbs aria-label="breadcrumb">
+        <Breadcrumbs aria-label="breadcrumb" separator="">
           <Link
-            style={{ textDecoration: "none", color: "black" }}
+            style={{ textDecoration: "none", color: "white" }}
             href="#machine-learning"
             onClick={(e) => e.preventDefault}
           >
-            Machine Learning
+            Machine Learning{" "}
+            <span style={{ color: "tomato", marginLeft: "2px" }}>/</span>
           </Link>
           <Link
-            style={{ textDecoration: "none", color: "black" }}
+            style={{ textDecoration: "none", color: "white" }}
             href="#fullstack"
             onClick={(e) => e.preventDefault}
           >
-            Full Stack
+            Full Stack{" "}
+            <span style={{ color: "tomato", marginLeft: "2px" }}>/</span>
           </Link>
           <Link
-            style={{ textDecoration: "none", color: "black" }}
+            style={{ textDecoration: "none", color: "white" }}
             href="#frontend"
             onClick={(e) => e.preventDefault}
           >

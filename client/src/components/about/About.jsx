d7324b8db258a16e946b003e739996b1f722356b
ikReza <ireza.kaiser00@gmail.com>
2020-11-17 17:08:28 +0600

    about section chnaged

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -1,37 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box, Typography, Grid, Avatar, Hidden } from "@material-ui/core";
+import { Box, Typography, Grid } from "@material-ui/core";
 
 import MainFooter from "../MainFooter";
-import { useStyles } from "./styles";
-
 import profileImg from "../assets/card.jpg";
-import { CONTENTS } from "./contents";
+
+import "./about.scss";
 
 //JSX
 const About = () => {
-  const classes = useStyles();
-
   return (
-    <Box component="div">
-      <Box component="div" className={classes.box1}>
+    <>
+      <div className="about-container">
         <Grid container justify="center">
           <Grid item xs={11} sm={8} md={9}>
-            <Box className={classes.card}>
-              <Hidden xsDown>
-                <img
-                  src={profileImg}
-                  alt="Ibrahim Kaiser"
-                  className={classes.circle}
-                />
-              </Hidden>
-              <Hidden smUp>
-                <img
-                  src={profileImg}
-                  alt="Ibrahim Kaiser"
-                  className={classes.mblCircle}
-                />
-              </Hidden>
+            <Box className="about-box">
+              <img
+                src={profileImg}
+                alt="Ibrahim Kaiser"
+                className="about-img"
+              />
               <Typography
                 variant="h5"
                 gutterBottom
@@ -39,95 +27,62 @@ const About = () => {
               >
                 Hi, This is Kaiser,
               </Typography>
-              <Typography component="p" gutterBottom className={classes.text}>
+              <Typography component="p" gutterBottom>
                 a graduate from Civil Engineering department of Bangladesh
                 University of Engineering and Technology (BUET). I live in
                 Dhaka, the capital of Bangladesh.
               </Typography>
               <br />
-              <Typography component="p" gutterBottom className={classes.text}>
-                Since my graduation, I have been investing time to add some
-                extra skills to my profile. I have learned Machine Learning
-                using python, MERN Stack for web development. I have finished
-                courses mainly from Udemy and Coursera and took help from some
-                other online resources e.g. StackOverflow, Youtube. Using these
-                skills I did some hands-on projects too which are added in the{" "}
+              <Typography component="p" gutterBottom>
+                First and foremost, I love writing code. I started with C++.
+                Nothing is more thrilling to me than seeing all the test cases
+                are green saying your code has been accepted. I use Codeforce
+                and Hackerrank to solve programming related problems.
+              </Typography>
+              <br />
+              <Typography component="p" gutterBottom>
+                At some point, I saw a video of web development from my YouTube
+                suggestions. That's the time when my JavaScript journey started.
+                How frontend, backend, and database all are interconnected, how
+                data passes from frontend to backend and it collects or stores
+                data in the database maintaining the security, that is
+                incredible. I love applying responsive web design principles and
+                watching my web pages shrink into the mobile screen and still
+                look amazing.
+              </Typography>
+              <br />
+              <Typography component="p" gutterBottom>
+                "Learn new things every day" is one of my mottos since my
+                graduation. So I watched some TED talks about machine learning
+                and took a course in Coursera and instantly fall in love with
+                it. I download various data from Kaggle and apply different
+                machine learning models to see what suits most. Some hands-on
+                machine learning projects is added in the{" "}
                 {
-                  <Link
-                    style={{ textDecoration: "none", color: "#66FCF1" }}
-                    to="/portfolio"
-                  >
+                  <Link style={{ color: "#66FCF1" }} to="/portfolio">
                     portfolio
                   </Link>
                 }{" "}
-                section.
+                section
               </Typography>
               <br />
-              <Typography component="p" gutterBottom className={classes.text}>
-                Apart from my academic life, I also enjoy sharing life updates
-                in my social networks. I have some other hobbies like -
-                travelling, dancing, photography.
+              <Typography component="p" gutterBottom>
+                My specialties include quickly learning new skills and
+                programming languages, problem-solving, responsive design
+                principles. So far I have Python, C++, JavaScript, HTML(5),
+                CSS(3), ReactJS, NodeJS, MongoDB, Machine Learning, Git/Github
+                under my belt. I have started learning GatsbyJS and PyTorch.
               </Typography>
               <br />
-              <Typography component="p" gutterBottom className={classes.text}>
+              <Typography component="p" gutterBottom>
                 Thanks for stopping by!
               </Typography>
             </Box>
           </Grid>
-          <Grid item xs={11} sm={8} md={9}>
-            <Box
-              style={{
-                padding: "10px",
-                margin: "3vh auto",
-                background: "#233",
-              }}
-              borderTop={5}
-            >
-              <Typography style={{ display: "block", color: "tomato" }}>
-                Things I love:
-              </Typography>
-              <Box
-                style={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "space-evenly",
-                }}
-              >
-                <div
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    flexWrap: "wrap",
-                  }}
-                >
-                  {CONTENTS.map((item, idx) => (
-                    <Box
-                      style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
-                      }}
-                      key={idx}
-                    >
-                      <Avatar
-                        alt={item.name}
-                        src={item.src}
-                        className={classes.avatar}
-                      />
-                      <Typography className={classes.text}>
-                        {item.name}
-                      </Typography>
-                    </Box>
-                  ))}
-                </div>
-              </Box>
-            </Box>
-          </Grid>
         </Grid>
-      </Box>
+      </div>
       <MainFooter />
-    </Box>
+    </>
   );
 };
 
